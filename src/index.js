7d83591630e6b38fdb43e274e940c06fddb61264
Bonnie <bonnie.hung@hotmail.ca>
2020-07-27 23:40:22 -0400

    starting point for creating svg

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import './index.css';
 class MyPage extends React.Component {
     render(){
         return(
-            <div>My Page</div>
+            <div id="simple-background">
+                <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+                    <line className="v-line" x1="50px" x2="50px" y1="0" y2="800px"/>
+                </svg>
+            </div>
         );
     }
 }
