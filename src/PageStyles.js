103fe4cabfc6d578ba7ba6a8ff42aafb1fa7a4a3
Bonnie <bonnie.hung@hotmail.ca>
2020-07-28 19:38:39 -0400

    added page styles object to hold style const values

---
+++ src/PageStyles.js
@@ -0,0 +1,8 @@
+const PageStyles = {
+    SIMPLE: "simple",
+    CLOUDY: "cloudy",
+    ANIMATED: "animated",
+    UNDECIDED: "undecided"
+};
+
+export default PageStyles;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PageStyles from './PageStyles';
 import './index.css';
 
 
 class MyPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            //default page style is SIMPLE
+            style: PageStyles.SIMPLE,
+        }
+    }
     render(){
         return(
-            <div id="simple-background">
+            <div id={this.state.style + "-background"}>
                 <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
                     <line className="v-line" x1="50px" x2="50px" y1="0" y2="800px"/>
                 </svg>
